b38f35e6487940d71a3ce7f70d6cbf0dffd00936
future-memory <johz@163.com>
2017-10-25 22:45:32 +0800

    fix

--- index.php
+++ index.php
@@ -1,22 +1,19 @@
 <?php
 
-define('SECURITY_IS_FROM_ENTRY', true);
 //应用路径
 define('APP_ROOT', dirname(__FILE__));
 //基础路径  （注意 不是框架路径）
 define('BASE_ROOT', dirname(__FILE__));
 
+//应用个性化配置
+//域名
+define('DOMAIN', 'https://www.qingchuzhang.com/');
+
+
 //获取全局配置
 $config = include(BASE_ROOT.'/config/main.php');
 //调用框架
 $nice  = BASE_ROOT.'/framework/nice.php';
-
-
-//应用个性化配置
-
-//域名
-define('DOMAIN', 'https://www.qingchuzhang.com/');
-
 
 require_once($nice);
 
