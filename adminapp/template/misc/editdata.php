5a0110607d0d259cdeeb6ad8a29022c4fb843e0f
future-memory <johz@163.com>
2017-10-28 21:11:35 +0800

    fix upload local

--- adminapp/template/misc/editdata.php
+++ adminapp/template/misc/editdata.php
@@ -242,7 +242,7 @@
       $.ajax({
         type: "POST",
         url: 'index.php?wants_json=true',
-        data: 'mod=attachment&action=upyun_sign&module=app',
+        data: 'mod=attachment&action=local_sign&module=misc&filename='+file_data.name,
         dataType: 'json',
         error: function(request) {
           is_uploading = false;
@@ -251,8 +251,7 @@
         success: function(res) {
           if(res && res.code==200){
 
-            form_data.append('signature', res.data.signs[0].signature);
-            form_data.append('policy', res.data.signs[0].policy);
+            form_data.append('filepath', res.data.filepath);
 
             $.ajax({
               url: res.data.url,
@@ -268,18 +267,11 @@
                 submit_alert('上传图片错误: ' + request.message);
               },
               success: function(_res){
-                _res = _res.replace(/image-/g, 'image_');
                 _res = JSON.parse(_res);
-                if (_res && _res.code == 200) {
-                  if (allow_pic_height && allow_pic_width && _res.image_height && _res.image_width) {
-                    if (false && (allow_pic_height != _res.image_height || allow_pic_width != _res.image_width)) {
-                      is_post = false;
-                      submit_alert('图片尺寸长宽不符合主题');
-                      return false;
-                    } else {
-                      post_data(_res.url);
-                    }
-                  }
+                if (_res && _res.data && _res.data.filepath) {
+                  post_data(_res.data.filepath);
+                }else{
+                  submit_alert(_res.error);
                 }
               }
              });
